be09b9ad1620b2efd0dab0bb68bfbdcabfbfc531
VSM Dave <dave@vsmedia.co.uk>
2017-12-24 11:28:06 +0000

    prepare for deploy

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,19 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from 'chat'
+  end
+
+  def send_message(payload)
+    message = Message.new(author: current_user, body: payload['message'])
+    if message.save
+      ActionCable.server.broadcast 'chat', message: render(message)
+    end
+  end
+
+  private
+
+  def render(message)
+    ApplicationController.new.helpers.c('message', message: message)
+  end
+end
--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,17 @@
+import { setCallback } from "client/chat";
 import "components/message/message"; // message is nested, so we import it here
 import "./messages.css";
+
+const messages = document.querySelector(".js-messages");
+const content = messages.querySelector(".js-messages--content");
+
+function scrollToBottom() {
+  content.scrollTop = content.scrollHeight;
+}
+
+scrollToBottom();
+
+setCallback(message => {
+  content.insertAdjacentHTML("beforeend", message);
+  scrollToBottom();
+});
